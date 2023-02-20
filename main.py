8464ae5c952cc99c94646bb13b21180250f3cf21
Airat Sakhibgareev <mr-airat@yandex.ru>
2023-02-20 22:35:58 +0300

    added sqlite database

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+con = sqlite3.connect('db.sqlite', check_same_thread=False)
+cur = con.cursor()
+
+def db_table_val(user_id: int, user_name: str):
+    cur.execute('CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY, user_name TEXT);')
+    cur.execute('INSERT INTO users (user_id, user_name) VALUES (?, ?)', (user_id, user_name))
+    con.commit()
+
 API_TOKEN = os.getenv('API_TOKEN')
-name = ''
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -16,8 +23,11 @@ def start_message(message):
     bot.register_next_step_handler(message, get_name)
 
 def get_name(message):
-    global name
-    name=message.text
-    bot.send_message(message.from_user.id, f'Hi, {name}')
+    us_id = message.from_user.id
+    us_name=message.text
+    bot.send_message(message.from_user.id, f'Hi, {us_name}')
+
+    db_table_val(user_id=us_id, user_name=us_name)
+
 
 bot.polling(none_stop=True, interval=0)
